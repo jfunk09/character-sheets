8b6febca57173e85703ae24b0a143d377d23383a
James Funk <jfunk09@gmail.com>
2017-02-25 23:22:46 -0800

    updating layout for character page and character creation page

--- app/components/CharacterSheet.js
+++ app/components/CharacterSheet.js
@@ -6,19 +6,22 @@ export default class CharacterSheet extends Component {
 	render () {
 		return (
 			<View style={styles.characterSheet}>
-				<Text>Character: {this.props.name}</Text>
+				<View style={styles.headerRow}>
+					<TouchableHighlight
+						style={styles.backButton}
+						onPress={this.props.toCharacterSelect}>
+						<Text style={styles.backButtonText}>&lt;</Text>
+					</TouchableHighlight>
+					<Text style={styles.characterName}>{this.props.name}</Text>
+					<TouchableHighlight
+						style={styles.deleteButton}
+						onPress={this.props.deleteCharacter.bind(null, this.props.name)}>
+						<Text style={styles.deleteButtonText}>&times;</Text>
+					</TouchableHighlight>
+				</View>
+				<View style={styles.statsDisplay}>
 
-				<Button
-					onPress={this.props.toCharacterSelect}
-					title="Back to character selection"
-					color="#BF8E04"
-				/>
-
-				<Button
-					onPress={this.props.deleteCharacter.bind(null, this.props.name)}
-					title="DELETE"
-					color="#BF0074"
-				/>
+				</View>
 			</View>
 		)
 	}
@@ -38,6 +41,34 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: '#bf342a',
+		backgroundColor: '#bfa859',
+	},
+	headerRow: {
+		flex:1,
+		flexDirection: 'row'
+	},
+	statsDisplay: {
+		flex: 11
+	},
+	backButton: {
+		flex: 1,
+		backgroundColor: '#ab9456'
+	},
+	backButtonText: {
+		fontSize: 32,
+		textAlign: 'center',
+	},
+	deleteButton: {
+		flex: 1,
+		backgroundColor: '#9d0610'
+	},
+	deleteButtonText: {
+		fontSize: 32,
+		textAlign: 'center'
+	},
+	characterName: {
+		flex: 6,
+		fontSize: 28,
+		textAlign: 'center'
 	}
 });
--- app/components/CreateCharacter.js
+++ app/components/CreateCharacter.js
@@ -1,39 +1,181 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text, StyleSheet, Navigator, TouchableHighlight, Button, TextInput } from 'react-native';
+import { View, Text, StyleSheet, Navigator, TouchableHighlight, Button, TextInput, Picker } from 'react-native';
+const Item = Picker.Item;
 
 export default class CreateCharacter extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			name: '',
-			inUse: false
+			strength: '3',
+			dexterity: '3',
+			constitution: '3',
+			intelligence: '3',
+			wisdom: '3',
+			charisma: '3'
 		}
 	}
 
 	render () {
+		function onStatChange(statKey, statValue) {
+			const update = {};
+			update[statKey] = statValue;
+			this.setState(update);
+		}
+
 		return (
 			<View style={styles.addSheet}>
-				<TextInput
-					placeholder="Enter character name here"
-					onChangeText={(text) => {this.setState({name: text, inUse: false})}}
-					style={this.state.inUse ? styles.inUse : styles.charNameField}
-				/>
+				<View style={styles.nameField}>
+					<TextInput
+						placeholder="Enter character name here"
+						onChangeText={(text) => {this.setState({name: text, inUse: false})}}
+						style={styles.charNameField}
+					/>
+				</View>
 
-				<Button
-					onPress={() => {
-						const inUse = this.props.create(this.state.name);
-						if (inUse) {
-							this.setState({inUse: true});
-						}
-					}}
-					title="Create"
-					color="#BF8E04"
-				/>
-				<Button
-					onPress={this.props.cancel}
-					title="Cancel"
-					color="#BF8E04"
-				/>
+				<View style={styles.statPickers}>
+					<Picker
+						style={styles.statPicker}
+						selectedValue={this.state.strength}
+						onValueChange={onStatChange.bind(this, 'strength')}>
+						<Item label="3" value="3" />
+						<Item label="4" value="4" />
+						<Item label="5" value="5" />
+						<Item label="6" value="6" />
+						<Item label="7" value="7" />
+						<Item label="8" value="8" />
+						<Item label="9" value="9" />
+						<Item label="10" value="10" />
+						<Item label="11" value="11" />
+						<Item label="12" value="12" />
+						<Item label="13" value="13" />
+						<Item label="14" value="14" />
+						<Item label="15" value="15" />
+						<Item label="16" value="16" />
+						<Item label="17" value="17" />
+						<Item label="18" value="18" />
+					</Picker>
+					<Picker
+						style={styles.statPicker}
+						selectedValue={this.state.dexterity}
+						onValueChange={onStatChange.bind(this, 'dexterity')}>
+						<Item label="3" value="3" />
+						<Item label="4" value="4" />
+						<Item label="5" value="5" />
+						<Item label="6" value="6" />
+						<Item label="7" value="7" />
+						<Item label="8" value="8" />
+						<Item label="9" value="9" />
+						<Item label="10" value="10" />
+						<Item label="11" value="11" />
+						<Item label="12" value="12" />
+						<Item label="13" value="13" />
+						<Item label="14" value="14" />
+						<Item label="15" value="15" />
+						<Item label="16" value="16" />
+						<Item label="17" value="17" />
+						<Item label="18" value="18" />
+					</Picker>
+					<Picker
+						style={styles.statPicker}
+						selectedValue={this.state.constitution}
+						onValueChange={onStatChange.bind(this, 'constitution')}>
+						<Item label="3" value="3" />
+						<Item label="4" value="4" />
+						<Item label="5" value="5" />
+						<Item label="6" value="6" />
+						<Item label="7" value="7" />
+						<Item label="8" value="8" />
+						<Item label="9" value="9" />
+						<Item label="10" value="10" />
+						<Item label="11" value="11" />
+						<Item label="12" value="12" />
+						<Item label="13" value="13" />
+						<Item label="14" value="14" />
+						<Item label="15" value="15" />
+						<Item label="16" value="16" />
+						<Item label="17" value="17" />
+						<Item label="18" value="18" />
+					</Picker>
+					<Picker
+						style={styles.statPicker}
+						selectedValue={this.state.intelligence}
+						onValueChange={onStatChange.bind(this, 'intelligence')}>
+						<Item label="3" value="3" />
+						<Item label="4" value="4" />
+						<Item label="5" value="5" />
+						<Item label="6" value="6" />
+						<Item label="7" value="7" />
+						<Item label="8" value="8" />
+						<Item label="9" value="9" />
+						<Item label="10" value="10" />
+						<Item label="11" value="11" />
+						<Item label="12" value="12" />
+						<Item label="13" value="13" />
+						<Item label="14" value="14" />
+						<Item label="15" value="15" />
+						<Item label="16" value="16" />
+						<Item label="17" value="17" />
+						<Item label="18" value="18" />
+					</Picker>
+					<Picker
+						style={styles.statPicker}
+						selectedValue={this.state.wisdom}
+						onValueChange={onStatChange.bind(this, 'wisdom')}>
+						<Item label="3" value="3" />
+						<Item label="4" value="4" />
+						<Item label="5" value="5" />
+						<Item label="6" value="6" />
+						<Item label="7" value="7" />
+						<Item label="8" value="8" />
+						<Item label="9" value="9" />
+						<Item label="10" value="10" />
+						<Item label="11" value="11" />
+						<Item label="12" value="12" />
+						<Item label="13" value="13" />
+						<Item label="14" value="14" />
+						<Item label="15" value="15" />
+						<Item label="16" value="16" />
+						<Item label="17" value="17" />
+						<Item label="18" value="18" />
+					</Picker>
+					<Picker
+						style={styles.statPicker}
+						selectedValue={this.state.charisma}
+						onValueChange={onStatChange.bind(this, 'charisma')}>
+						<Item label="3" value="3" />
+						<Item label="4" value="4" />
+						<Item label="5" value="5" />
+						<Item label="6" value="6" />
+						<Item label="7" value="7" />
+						<Item label="8" value="8" />
+						<Item label="9" value="9" />
+						<Item label="10" value="10" />
+						<Item label="11" value="11" />
+						<Item label="12" value="12" />
+						<Item label="13" value="13" />
+						<Item label="14" value="14" />
+						<Item label="15" value="15" />
+						<Item label="16" value="16" />
+						<Item label="17" value="17" />
+						<Item label="18" value="18" />
+					</Picker>
+				</View>
+
+				<View style={styles.buttons}>
+					<Button
+						onPress={() => {
+							this.props.create(this.state.name);
+						}}
+						title="Create"
+						color="#BF8E04"
+					/>
+					<Button
+						onPress={this.props.cancel}
+						title="Cancel"
+						color="#BF8E04"
+					/>
+				</View>
 			</View>
 		)
 	}
@@ -54,11 +196,24 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		backgroundColor: '#bf342a',
 	},
-	charNameField: {
-		width: 200
+	nameField: {
+		flex: 1,
+		flexDirection: 'row'
 	},
-	inUse: {
-		width: 200,
-		color: '#bf090d'
+	charNameField: {
+		flex: 1
+	},
+	statPickers: {
+		flex: 2,
+		flexDirection: 'row'
+	},
+	statPicker: {
+		flex: 1,
+		color: '#000000'
+	},
+	buttons: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
 	}
 });
